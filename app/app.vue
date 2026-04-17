73959ce6edfd0eaaab7cfa400ba89536c7b1e18c
DuodecimAD <tonyxii@gmail.com>
2026-04-18 00:17:07 +0200

    init nuxt.js + graph.js

---
+++ app/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+        <h1>Dashboard</h1>
+        <LineChart />
+  </div>
+</template>
+
+<script setup>
+import LineChart from '~/components/LineChart.vue'
+</script>
---
+++ app/components/LineChart.vue
@@ -0,0 +1,27 @@
+<template>
+  <Line :data="data" />
+</template>
+
+<script setup>
+import { Line } from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement
+} from 'chart.js'
+
+ChartJS.register(LineElement, CategoryScale, LinearScale, PointElement)
+
+const data = {
+  labels: ['Jan', 'Feb', 'Mar', 'Apr'],
+  datasets: [
+    {
+      label: 'Users',
+      data: [10, 20, 15, 30],
+      borderColor: 'blue'
+    }
+  ]
+}
+</script>
